65d00d51dad1981d063930f56dc8e7e1bed82464
sleepercell38 <insanevishal07@gmail.com>
2024-12-24 14:00:38 +0530

    updated

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const http= require("http");  // the work of the require statement is to import the http module from the node.js library (node modules)
+
+const server= http.createServer((req, res)=>{     //here we create the server using the create server method
+    if(req.url=="/"){
+          res.end("welcome to homepage")
+    }
+    if(req.url=="/about"){
+        res.end("welcome to about page")
+    }
+})
+server.listen(3000)
