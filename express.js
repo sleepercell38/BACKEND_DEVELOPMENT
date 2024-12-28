a365a82fccaf67a85a856fe63f26a6701e3106a6
sleepercell38 <insanevishal07@gmail.com>
2024-12-28 18:28:08 +0530

    updated

--- express.js
+++ express.js
@@ -4,26 +4,29 @@ const app= express()
 
 app.use((req,res,next)=>{
     console.log("this is a middle ware")  //basically this function would run everytime we hit a route or request to the server
-    return next()       //basically middle ware has three times of parameters 
+    return next()                            //basically middle ware has three times of parameters 
     
 })
-app.use(morgan("dev"))  //using of a third party middleware
+app.use(morgan("dev"))                                     //using of a third party middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))               //using built in middleware functions
+app.use(express.static("public"))
 
-app.set("view engine","ejs")    //using of ejs to render a html file over the sever 
+app.set("view engine","ejs")                    //using of ejs to render a html file over the sever 
   
 
 
 app.get('/',(req,res)=>{
-    res.render("index")         //rendering the index file
-    // res.send("welcome to homepage")
+    res.render("index")                            //rendering the index file
+
 })
 
 app.get('/about',(req,res)=>{
-   res.send("this is aabout page")        //rendering the index2 file the response will be the same as the index file
-    // res.send("welcome to about page")
+   res.send("this is about page")                 //rendering the index2 file the response will be the same as the index file
+     
 })
-app.get("/get-form-data",(req,res)=>{
-   console.log(req.query)
+app.post("/formdata",(req,res)=>{
+   console.log(req.body)
     res.send("data recieved")
 })
 
